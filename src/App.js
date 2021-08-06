953f2ecaa46d524a249254c409ee9ba00eadca83
jungiwon <iwxx21@naver.com>
2021-08-06 17:54:19 +0900

    210806_v2 react CRUD with 생활코딩 ( 컴포넌트 이벤트 생성)

--- src/App.js
+++ src/App.js
@@ -1,16 +1,55 @@
 import React, { Component } from 'react';
 import './App.css';
 
+// 분리한 component import
+import Content from './components/Content';
+import TOC from './components/TOC';
+import Subject from './components/Subject';
+
 
 
 class App extends Component {
+  constructor(props) { // 초기화
+    super(props);
+    this.state = {
+      mode:'welcome',
+      Subject: {title: 'WEB', sub:'World Wide Web!!!'},
+      welcome:{title: 'welcome', desc:'Hello, React!'},
+      Content: [
+        {id:1, title:'HTML', desc:'HTML is HyperText...'},
+        {id:2, title:'CSS', desc:'CSS is for design...'},
+        {id:3, title:'JavaScript', desc:'JavaScript is for interactive...'},
+      ]
+    }
+  }
+
   render() {
+    let _title, _desc = null;
+    if(this.state.mode === 'welcome') {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if(this.state.mode === 'read') {
+      _title = this.state.Content[0].title;
+      _desc = this.state.Content[0].desc;
+    }
+
     return (
       <div className="App">
-        Hello, React!!!
-        <Subject title="WEB" sub="World Wide Web!!!"></Subject>
-        <TOC></TOC>
-        <Content title='HTML1' desc='HTML is HyperText Markup Language.1'></Content>
+        <Subject 
+          title={this.state.Subject.title} 
+          sub={this.state.Subject.sub}
+          onChangePage={function(){
+            this.setState({mode:'welcome'});
+          }.bind(this)}
+          >
+        </Subject>
+        <TOC 
+          onChangePage={function(){
+            this.setState({mode:'read'});
+        }.bind(this)} 
+        data={this.state.Content}
+        ></TOC>
+        <Content title={_title} desc={_desc}></Content>
         
       </div>
 
--- src/components/TOC.js
+++ src/components/TOC.js
@@ -3,15 +3,31 @@ import React, { Component } from 'react';
 
 class TOC extends Component {
     render() {
+        let data = this.props.data;
+        let lists = [];
+        let i = 0;
+        while(i < data.length) {
+            lists.push(
+            <li key={data[i].id}>
+                <a href={'/content/'+data[i].id}
+                    onClick={function(e){
+                    e.preventDefault();
+                    this.props.onChangePage();
+                    }.bind(this)}>
+                    {data[i].title}
+                </a> 
+             </li>);
+
+            i++;
+        }
+
       return(
         <nav>
             <ul> 
-                <li><a href="1.html">HTML</a></li>
-                <li><a href="2.html">CSS</a></li>
-                <li><a href="3.html">JavaScript</a></li>
+                {lists}
             </ul>
         </nav>
-      );
+      );    
     }
   }
 
